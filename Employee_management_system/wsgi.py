03e0ce06d4afada6778df08bef2faf0bde96d2ad
Aman-Deepak <deepak.aman66@gmail.com>
2021-12-31 22:19:38 +0530

    testing

--- Employee_management_system/wsgi.py
+++ Employee_management_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Employeem project.
+WSGI config for Employee_management_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Employeem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Employee_management_system.settings')
 
 application = get_wsgi_application()
